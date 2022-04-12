66aa18cd36d975aba9caa1d13d7c592846d69678
GLMaxime <maxime.gauthey@gmail.com>
2022-04-12 14:39:27 +0200

    premiere version

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -11,21 +11,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Button button;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override // fonction déclaré apres remplace une autre qui porte le meme nom
+    protected void onCreate(Bundle savedInstanceState) { //methode déclaré
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.capturedImage);
         button = findViewById(R.id.openCamera);
 
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            @Override // fonction déclaré apres remplace une autre qui porte le meme nom
+            public void onClick(View view) { //methode déclaré
                 Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(open_camera, 100);
 
@@ -33,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
             });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    @Override // fonction déclaré apres remplace une autre qui porte le meme nom
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { //methode
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap photo = (Bitmap)data.getExtras().get("data");
         imageView.setImageBitmap(photo);
